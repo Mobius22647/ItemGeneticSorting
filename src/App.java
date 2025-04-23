9d94f8593b6252a5d6fd283ec47b2c40b319f3cc
Alex McDaniel <alexmcdaniel2168@gmail.com>
2025-04-23 00:40:20 -0400

    Updated Arraylist

--- src/App.java
+++ src/App.java
@@ -3,21 +3,40 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class App {
 
-    public static List<List<String>> Combo(List<List<String>> records){ //Only combos of 10 items with Weights below 120
+    public static List<List<String>> Combo(List<List<String>> records){ //Only combos of 10 items with Weights below 100
         
-        List<List<String>> Best = new ArrayList<>();
-        for (int i = 0; i < records.size(); i++) {
-            System.out.println(records.get(i));
-        }
+        
+        ArrayList<List<String>> Best = new ArrayList<>();
+        Double Value = 0.0;
+        Double Weight = 0.0;
+        Collections.shuffle(records);
+        
+        for (int j = 0; j < 20; j++ ) {
+            for (int i = 0; i < 10; i++) {
+                
+                Value += Double.parseDouble(records.get(i).get(1));
+                Weight += Double.parseDouble(records.get(i).get(2));
+                System.out.println(records.get(i));
+                Best.add(records.get(i));
+            }
+            if (Weight > 100){
+                Value = 0.0;
+            }
 
+            
+            System.out.println(Weight);
+            System.out.println(Value);
+           
+        }
         return records;
     }
     public static void main(String[] args) {
-        String csvFile = "random_items.csv";
+        String csvFile = "src\\random_items.csv";
         String line;
         String csvSplitBy = ",";
         List<List<String>> records = new ArrayList<>();
---
+++ src/ListSort.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSort {
+    ArrayList<List<String>> generation;
+    Double Value;
+    Double Weight;
+
+    // Constructor
+    public ListSort(ArrayList<List<String>> generation, Double Value, Double Weight) {
+        this.generation = generation;
+        this.Value = Value;
+        this.Weight = Weight;
+
+    }
+}
